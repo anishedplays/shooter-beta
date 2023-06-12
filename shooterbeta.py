d6cabe392df0b911ca47b74bb2918ccaca0e8b7a
anishedplays <74805469+anishedplays@users.noreply.github.com>
2023-06-12 08:23:45 -0400

    sw

--- shooterbeta.py
+++ shooterbeta.py
@@ -8,13 +8,9 @@ import button
 mixer.init()
 pygame.init()
 
-pygame.display.set_mode()
 
-window_width = pygame.display.get_surface().get_width()
-window_height = pygame.display.get_surface().get_height()
-
-SCREEN_WIDTH = window_width
-SCREEN_HEIGHT = window_height
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Shooter')
